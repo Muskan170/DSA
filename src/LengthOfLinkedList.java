88119a264c37a4862d109e241791d1a0808d6ea7
Muskan Ranjan <19410121136.e.muskanranjan@gmail.com>
2025-08-15 21:39:36 +0530

    chore: questions

---
+++ src/LengthOfLinkedList.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.List;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1, Node next1) {
+        this.data = data1;
+        this.next = next1;
+    }
+
+    Node(int data1) {
+        this.data = data1;
+        this.next = null;
+    }
+}
+
+public class LengthOfLinkedList {
+    public static int lengthOfList(Node head) {
+        int cnt = 0;
+        while (head != null) {
+            cnt++;
+            head = head.next;
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> arr = Arrays.asList(12, 8, 5, 7);
+
+        Node head = new Node(arr.get(0));
+        head.next = new Node(arr.get(1));
+        head.next.next = new Node(arr.get(2));
+        head.next.next.next = new Node(arr.get(3));
+
+        int res = lengthOfList(head);
+
+        System.out.println(res);
+    }
+}
---
+++ src/SearchElementInSingleLL.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.List;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1, Node next1) {
+        this.data = data1;
+        this.next = next1;
+    }
+
+    Node(int data1) {
+        this.data = data1;
+        this.next = null;
+    }
+}
+
+public class SearchElementInSingleLL {
+    public static boolean searchElement(Node head, int ele) {
+        if (head == null) {
+            return false;
+        }
+        while(head != null) {
+            if (head.data == ele) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        List<Integer> arr = Arrays.asList(12, 8, 5, 7);
+
+        Node head = new Node(arr.get(0));
+        head.next = new Node(arr.get(1));
+        head.next.next = new Node(arr.get(2));
+        head.next.next.next = new Node(arr.get(3));
+
+        boolean res = searchElement(head, 23);
+        System.out.println(res);
+    }
+}
